21b65814abe4206c56dbe24c5817362e231b458d
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-07 19:43:30 -0300

    feat: service login gerando token

--- app/backend/src/services/login.ts
+++ app/backend/src/services/login.ts
@@ -1,17 +1,20 @@
 import * as bcrypt from 'bcryptjs';
 import IUser from '../protocols/IUser';
 import User from '../database/models/user';
+import jwtUtils from '../utils/jsonWebToken';
 
 export default class LoginService {
   private model = User;
 
-  public async sigIn(email: string, password: string): Promise<string> {
-    const user = await this.model.findOne({ where: { email } }) as IUser;
+  public async sigIn(emailRequest: string, password: string): Promise<string> {
+    const user = await this.model.findOne({ where: { email: emailRequest } }) as IUser;
 
     const isValid = await bcrypt.compare(password, user.password);
-
     if (!isValid) console.log('deu ruim');
 
-    return 'ola';
+    const { id, email, username, role } = user;
+    const token = jwtUtils.decode({ id, email, username, role });
+
+    return token;
   }
 }
--- app/backend/src/utils/jsonWebToken.ts
+++ app/backend/src/utils/jsonWebToken.ts
@@ -1,13 +1,18 @@
 import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
 import IUser from '../protocols/IUser';
 
+dotenv.config();
+
 const decode = (payload: Omit<IUser, 'password'>) => {
+  const secret: jwt.Secret = process.env.JWT_SECRET as string;
+
   const jwtConfig: jwt.SignOptions = {
     expiresIn: '7d',
     algorithm: 'HS256',
   };
 
-  const token = jwt.sign({ data: payload }, 'a', jwtConfig);
+  const token = jwt.sign({ data: payload }, secret, jwtConfig);
 
   return token;
 };
