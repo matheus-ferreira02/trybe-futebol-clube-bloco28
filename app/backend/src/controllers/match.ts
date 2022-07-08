1c6442e4cbcd3113ab5a46b3d008d9a073dad6b4
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-08 17:03:27 -0300

    feat: create match na controller

--- app/backend/src/controllers/match.ts
+++ app/backend/src/controllers/match.ts
@@ -9,4 +9,13 @@ export default class MatchController {
 
     return res.status(200).json(matches);
   }
+
+  public async create(req: Request, res: Response) {
+    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = req.body;
+
+    const createdMatch = await this.service
+      .create({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals });
+
+    return res.status(200).json(createdMatch);
+  }
 }
