d7042967c1d89cf624f96806d51a6bcf3b9c3535
Matheus Ferreira <m.ferreira.araujo2016@gmail.com>
2022-07-08 16:05:14 -0300

    feat: service de team criada

--- app/backend/src/services/team.ts
+++ app/backend/src/services/team.ts
@@ -1,1 +1,12 @@
-expot default 
+import ITeam from '../protocols/ITeam';
+import Team from '../database/models/team';
+
+export default class TeamService {
+  private model = Team;
+
+  public async get(): Promise<ITeam[]> {
+    const teams = await this.model.findAll() as ITeam[];
+
+    return teams;
+  }
+}
